8911c87f4fe8716aea9398734de87d14f14646c7
sabbir-hossen66 <sabbir.ms2000@gmail.com>
2025-12-20 22:50:01 +0600

    increase size

---
+++ module-1/dynamic_array_size_increase.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int a[3];
+    for(int i=0;i<3;i++){
+        cin >> a[i];
+    }
+    int b[5];
+    for (int i = 0; i < 3; i++)
+    {
+        b[i]=a[i];
+    }
+    b[3]=40;
+    b[4]=50;
+    for (int i = 0; i < 5; i++)
+    {
+        cout << b[i] << " ";
+    }
+    return 0;
+    
+}
